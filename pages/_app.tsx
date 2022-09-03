b05f319eadf563f0e356072cd160f49117deba7a
Denis <like-nix@mail.ru>
2022-09-03 22:12:30 +0300

    simple dark mode switcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,32 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
+import React from "react";
+import type { AppProps } from "next/app";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import GlobalStyle from "../components/globalstyles";
 
-const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
-}
-
+const themeLigth: DefaultTheme = {
+    colors: {
+        primary: "#6b6b6b",
+        secondary: "#7e96ff",
+    },
+};
+const themeDark: DefaultTheme = {
+    colors: {
+        primary: "#161616",
+        secondary: "#0070f3",
+    },
+};
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+    const [theme, setTheme] = React.useState(true);
+    return (
+        <>
+            <ThemeProvider theme={theme ? themeLigth : themeDark}>
+                <GlobalStyle />
+                <div>
+                    <button onClick={() => setTheme(!theme)}>Toggle</button>
+                    <span>Current: {theme? "Light": "Dark"}</span>
+                </div>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
