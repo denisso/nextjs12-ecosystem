efeb063acce0f60fdd0d4ccff597dccc1c767308
Denis <like-nix@mail.ru>
2022-09-17 00:03:20 +0300

    start with preloader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { Context } from "../Content";
 import Modal from "../components/Modal";
 function MyApp({ Component, pageProps }: AppProps) {
-    const [modal, setModal] = React.useState(false);
+    const [modal, setModal] = React.useState(true);
     return (
         <>
             <Context.Provider value={{ modal, setModal }}>
