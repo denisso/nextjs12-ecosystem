f08edcb55061be9fa2920772101388e7e9cb4261
Denis <like-nix@mail.ru>
2022-11-13 18:03:24 +0300

    commit case

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,3 +1,15 @@
-const Index = () => <>Posts Index</>;
+import { useRouter } from "next/router";
+import React from "react";
+
+const Index = () => {
+    const router = useRouter();
+    const [query, setQuery] = React.useState("");
+    React.useEffect(() => {
+        if (!router.isReady) return;
+        setQuery(JSON.stringify(router.query));
+    }, [router.isReady, router.query, setQuery]);
+
+    return <>Posts Index query: {query}</>;
+};
 
 export default Index;
